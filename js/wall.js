8a497c689e6e811451217a956c509a81ec378e81
omer1228 <oigba2105@withs.org>
2016-03-18 14:17:40 +0000

    Facebook wall project

--- js/wall.js
+++ js/wall.js
@@ -2,5 +2,16 @@ $(document).ready(function()
 {
 	$('#message').focus();
 
-	// YOUR CODE GOES HERE
+
+$("#post-btn").click(function(){
+  
+  
+  
+  var word = $("#message").val();
+  $("#wall").append("<div>" + "<img src='http://www.scass.ae/en/About1/PublishingImages/Pages/staff/nophoto.jpg'>" + word +"</div>");
+  
+});
+
+	
+
 });
